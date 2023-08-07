a052996b40d8ba5492e7b1c5ad367309ae59619c
BWYC <129849126+BWYC@users.noreply.github.com>
2023-08-07 13:26:54 +0000

    Update 7 files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Canvas } from "react-three-fiber";
 import { Physics, usePlane, useBox } from "@react-three/cannon";
 import "./styles.css";
 import Controls from "./Controls";
+import Tree from "./Tree";
 
 function Plane(props) {
   const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
@@ -16,12 +17,12 @@ function Plane(props) {
 
 function Cube(props) {
   const [ref] = useBox(() => ({
-    mass: 1,
+    mass: 5,
     position: [0, 5, 0],
-    rotation: [0.4, 0.2, 0.5],
-    ...props
+    rotation: [0.1, 0.2, 0.8],
+    ...props,
   }));
-  const color = props.color ? props.color : "hotpink";
+  const color = props.color ? props.color : "gray";
   return (
     <mesh receiveShadow castShadow ref={ref}>
       <boxBufferGeometry />
@@ -35,23 +36,24 @@ function App() {
     <Canvas
       shadowMap
       sRGB
-      gl={{ alpha: false }}
-      camera={{ position: [-1, 1, 5], fov: 50 }}
+      gl={{ alpha: true }}
+      camera={{ position: [3, 1, 10], fov: 50 }}
     >
-      <color attach="background" args={["lightblue"]} />
+      <color attach="background" args={["skyblue"]} />
       <Physics>
         <Controls />
-        <hemisphereLight intensity={0.35} />
+        <hemisphereLight intensity={0.1} />
         <spotLight
-          position={[10, 10, 10]}
+          position={[30, 10, 10]}
           angle={0.3}
-          penumbra={1}
+          penumbra={0.5}
           intensity={2}
           castShadow
         />
         <Plane />
         <Cube />
-        <Cube position={[0, 10, -2]} color="rebeccapurple" />
+        <Cube position={[0, 10, -2]} color="gray" />
+
         <Cube position={[0, 20, -2]} color="darkseagreen" />
       </Physics>
     </Canvas>
--- src/Controls.js
+++ src/Controls.js
@@ -100,6 +100,7 @@ const Controls = () => {
       }}
       ref={controlsRef}
     />
+  
   );
 };
 
